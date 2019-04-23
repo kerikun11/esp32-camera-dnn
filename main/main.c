50f27c04fe18a5af1b7987cd71664792bc78a702
Ryotaro Onuki <kerikun11+github@gmail.com>
2019-04-23 22:48:03 +0900

    Change Pin Assign to M5Stack Camera Module

--- main/main.c
+++ main/main.c
@@ -26,7 +26,7 @@ static const char* TAG = "camera";
 //M5STACK_CAM PIN Map
 #define CAM_PIN_RESET   15 //software reset will be performed
 #define CAM_PIN_XCLK    27
-#define CAM_PIN_SIOD    22
+#define CAM_PIN_SIOD    25
 #define CAM_PIN_SIOC    23
 
 #define CAM_PIN_D7      19
@@ -36,9 +36,9 @@ static const char* TAG = "camera";
 #define CAM_PIN_D3      5
 #define CAM_PIN_D2      34
 #define CAM_PIN_D1      35
-#define CAM_PIN_D0      32
+#define CAM_PIN_D0      17
 
-#define CAM_PIN_VSYNC   25
+#define CAM_PIN_VSYNC   22
 #define CAM_PIN_HREF    26
 #define CAM_PIN_PCLK    21
 
